14c65f61ad17f46ab16d40533cc8098a00062dbc
Johnny Cusicanqui <jfcusicanqui@gmail.com>
2018-02-04 16:50:28 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,16 +1,22 @@
 ##Reading data
-#setwd("N:/Explanatory_analysis") #set your working directory (where you unzipped the data)
-data <- read.table("./household_power_consumption.txt" , header=T, sep=";", na.strings="?")
-data1 <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
-dates <- as.character(data1$Date)
-times <- as.character(data1$Time)
-data1$Date <- strptime(dates, "%d/%m/%Y")
-data1$Time <- strptime(paste(dates, times), "%d/%m/%Y %H:%M:%S")
+setwd("N:/Explanatory_analysis") #your working directory
+colnames <- read.table("./household_power_consumption.txt", nrows=1, sep=";", header=F)
+cnames <- as.vector(t(colnames))
+##ids <- grep("1/2/2007", readLines("./household_power_consumption.txt"), fixed=T) #used to determine the range
+##ids2 <- grep("3/2/2007", readLines("./household_power_consumption.txt"), fixed=T) # from 1/2 to 3/2/2007
+ids <- 66638
+ids2 <- 69518
+numrows <- ids2[1] - ids[1]
+data <- read.table("./household_power_consumption.txt", skip=ids[1]-1, nrows=numrows, col.names= cnames, sep=";",na.strings="?")
+dates <- as.character(data$Date)
+times <- as.character(data$Time)
+data$Date <- strptime(dates, "%d/%m/%Y")
+data$Time <- strptime(paste(dates, times), "%d/%m/%Y %H:%M:%S")
 
 ##Plotting
 par(mar=c(3,4.4,2,2))
-with(data1, plot(Time, Sub_metering_1, type="n", ylab="Energy sub metering"))
-with(data1, lines(Time, Sub_metering_1))
-with(data1, lines(Time, Sub_metering_2, col="red"))
-with(data1, lines(Time, Sub_metering_3, col="blue"))
+with(data, plot(Time, Sub_metering_1, type="n", ylab="Energy sub metering"))
+with(data, lines(Time, Sub_metering_1))
+with(data, lines(Time, Sub_metering_2, col="red"))
+with(data, lines(Time, Sub_metering_3, col="blue"))
 legend("topright", lty=c(1,1,1), col=c("black", "red", "blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
